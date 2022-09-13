e3fbfb00d3196efd5e12f534d88d38d9228a03b3
pajleira <104856788+pajleira@users.noreply.github.com>
2022-09-13 13:11:07 +0800

    Update adopisoft.sh

--- adopisoft.sh
+++ adopisoft.sh
@@ -6,8 +6,8 @@ sudo apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs python nginx bind9 isc-dhcp-server
 sudo apt-get install -y hostapd dmidecode build-essential openssh-server python-pip unzip bridge-utils
-wget -O /tmp/adopisoft-5.1.2-08302022-amd64-node-v16.4.0.deb https://github.com/AdoPiSoft/Releases/releases/download/v5.1.2/adopisoft-5.1.2-08302022-amd64-node-v16.4.0.deb
-sudo apt-get install ./adopisoft-5.1.2-08302022-amd64-node-v16.4.0.deb
+wget -O /tmp/adopisoft-5.1.1-amd64-node-v16.4.0.deb https://github.com/AdoPiSoft/Releases/releases/download/v5.1.1/adopisoft-5.1.1-amd64-node-v16.4.0.deb
+sudo apt-get install /tmp/adopisoft-5.1.1-amd64-node-v16.4.0.deb
 
 echo "Create posgressql account:"
 wget -O ado-psql-script.sh https://gist.githubusercontent.com/alenteria/791dbe32175a01d1f1b602b25489ad22/raw/9a5aa879ac70d24bd9a7dd7f8ed97d7fe2c2f597/ado-psql-script.sh 
